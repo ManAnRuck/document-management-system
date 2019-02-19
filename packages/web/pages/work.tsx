e8e8b425c541b5f3b7105e1291f04ae31695db0b
Manuel Ruck <github@manuelruck.de>
2019-02-19 17:57:15 +0100

    add last merge changes

--- packages/web/pages/work.tsx
+++ packages/web/pages/work.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Page from '../layouts/main';
-import { TestComponent } from '@myproject/ui';
+import { TestComponent } from '@docms/ui';
 
 export default () => (
   <Page>
